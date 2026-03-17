8ec51f42aab3968e49c83c4f497dc1fc63123896
Anwesha-code <anweshasingh0611@gmail.com>
2026-03-17 21:45:57 +0530

    V1.1.0: Add new features:Multivariable template, parameterized jobs

--- scripts/generate_jobs.py
+++ scripts/generate_jobs.py
@@ -1,3 +1,5 @@
+from urllib import response
+
 import yaml
 import requests
 from jinja2 import Template
@@ -6,13 +8,16 @@ JENKINS_URL = "http://localhost:8080"
 USERNAME = "AnweshaSingh"
 API_TOKEN = "110d63401eb9d9937b6df043e73363cd6b"
 
+
 def load_template():
     with open("../templates/job_template.xml") as f:
         return Template(f.read())
 
+
 def load_jobs():
     with open("../configs/jobs.yaml") as f:
         return yaml.safe_load(f)["jobs"]
+
 
 def create_job(job_name, config_xml):
     crumb_url = f"{JENKINS_URL}/crumbIssuer/api/json"
@@ -40,9 +45,12 @@ def create_job(job_name, config_xml):
     )
 
     if response.status_code == 200:
-        print(f"✔ {job_name} created")
+        print(f"{job_name} created successfully")
     else:
-        print(f"✘ Failed {job_name}")
+        print(f"Failed {job_name} (Status: {response.status_code})")
+        print(response.text)
+
+
 def main():
     template = load_template()
     jobs = load_jobs()
@@ -50,6 +58,7 @@ def main():
     for job in jobs:
         config_xml = template.render(**job)
         create_job(job["name"], config_xml)
+
 
 if __name__ == "__main__":
     main()
